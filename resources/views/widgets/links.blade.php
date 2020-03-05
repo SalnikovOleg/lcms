1366b9a53c3386c5566fff6e6dccbaa3f32f4e96
SalnikovOleg <oleg_salnikov@ukr.net>
2020-03-05 23:18:19 +0200

    links widget

--- resources/views/widgets/links.blade.php
+++ resources/views/widgets/links.blade.php
@@ -1,2 +1,4 @@
-{-- widget menu, method list . view link list --}
-listcof links
+{{-- widget menu, method list . view link list --}}
+@foreach($items as $item)
+<div><a href="/{{$item['url']}}">{{$item['name']}}</a></div>
+@endforeach
